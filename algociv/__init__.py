f7323a39d7c9ab5ab5ca5107f788e824c317742d
ianbotashev <yanbotashev@gmail.com>
2020-07-31 00:37:38 -0700

    Updated the formatting of versions.

--- algociv/__init__.py
+++ algociv/__init__.py
@@ -1,9 +1,9 @@
 # Versions follow this simple format of their number:
-# DEVELOPMENT VERSION-MAJOR.MINOR.BUG FIXES/OPTIMIZATIONS
-# So for example, ALPHA 1.2.4 means:
+# MAJOR.MINOR.BUG FIXES/OPTIMIZATIONS-DEVELOPMENT VERSION
+# So for example, 1.2.4-alpha means:
 # - This is currently in alpha
 # - There has been one major release,
 # - There has been 2 minor releases associated with the major release 1.
 # - There are 4 bug fixes/optimizations associated with the minor release 2.
 
-__version__ = "ALPHA-1.0.0"
+__version__ = "1.0.0-alpha"
