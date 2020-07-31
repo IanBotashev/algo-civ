2a592b07c599dd6504fcfd4194f96c38f779ec37
ianbotashev <yanbotashev@gmail.com>
2020-07-31 00:33:51 -0700

    Added a version! Current version: ALPHA-1.0.0

---
+++ algociv/__init__.py
@@ -0,0 +1,9 @@
+# Versions follow this simple format of their number:
+# DEVELOPMENT VERSION-MAJOR.MINOR.BUG FIXES/OPTIMIZATIONS
+# So for example, ALPHA 1.2.4 means:
+# - This is currently in alpha
+# - There has been one major release,
+# - There has been 2 minor releases associated with the major release 1.
+# - There are 4 bug fixes/optimizations associated with the minor release 2.
+
+__version__ = "ALPHA-1.0.0"
